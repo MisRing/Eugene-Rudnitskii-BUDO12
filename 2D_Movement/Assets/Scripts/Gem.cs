4988b807a17b01956647120df6e872d94a2a3f47
MisRing <39882750+MisRing@users.noreply.github.com>
2025-10-09 18:38:32 +0300

    Scene changed

--- 2D_Movement/Assets/Scripts/Gem.cs
+++ 2D_Movement/Assets/Scripts/Gem.cs
@@ -6,11 +6,9 @@ public class Gem : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision == null)
-            return;
-        if (collision.gameObject.tag != "Player")
-            return;
-
-        Destroy(gameObject);
+        if (collision != null && collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
