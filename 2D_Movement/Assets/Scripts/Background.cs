956a601a76ab0e35d54ef9a878aa5b05a6e476aa
MisRing <39882750+MisRing@users.noreply.github.com>
2025-10-12 20:30:16 +0300

    Background

---
+++ 2D_Movement/Assets/Scripts/Background.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private bool _moveX, _moveY;
+    [SerializeField] private float _moveForce = 0.01f;
+    private Transform _cameraTransform;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_moveX && !_moveY)
+            return;
+
+            Vector2 position = _cameraTransform.position * _moveForce * (-1);
+        if (!_moveX)
+            position.x = 0;
+        if (!_moveY)
+            position.y = 0;
+
+        transform.position = position;
+    }
+}
