69ef69600d59159c0a30c65aaab40b3a640aef04
MisRing <39882750+MisRing@users.noreply.github.com>
2025-12-05 16:47:42 +0300

    Debug draw changed

--- Pedestrian System/Assets/Editor/WaypointDebugDraw.cs
+++ Pedestrian System/Assets/Editor/WaypointDebugDraw.cs
@@ -10,25 +10,41 @@ public class WaypointDebugDraw
         float visualMod = 1f;
         if((gizmoType & GizmoType.Selected) == 0)
         {
-            visualMod = 0.75f;
+            visualMod = 0.85f;
         }
 
-        Gizmos.color = Color.yellow * visualMod;
+        Gizmos.color = Color.darkRed * visualMod;
         Gizmos.DrawSphere(point.transform.position, 0.2f * visualMod);
 
         Handles.color = Color.white * visualMod;
-        Handles.DrawWireDisc(point.transform.position, Vector3.up, point.Radius, 4f * visualMod);
+        Handles.DrawWireDisc(point.transform.position, Vector3.up, point.Radius, 5f * visualMod);
+
+        Handles.color = Color.orangeRed * visualMod;
         Handles.DrawLine(   point.transform.right * point.Radius + point.transform.position,
                             -point.transform.right * point.Radius + point.transform.position,
-                            3f * visualMod);
+                            2f * visualMod);
 
-        Handles.DrawLine(   point.transform.right * 0.25f + point.transform.position,
-                            point.transform.forward * 0.45f + point.transform.position,
-                            3f * visualMod);
-        Handles.DrawLine(   -point.transform.right * 0.25f + point.transform.position,
-                            point.transform.forward * 0.45f + point.transform.position,
-                            3f * visualMod);
-        
+        Handles.color = Color.yellow * visualMod;
+
+        Vector3 offset = point.transform.position + point.transform.forward * 0.35f;
+
+        Handles.DrawLine(   point.transform.right * 0.25f + offset,
+                            point.transform.forward * 0.35f + offset,
+                            5f * visualMod);
+        Handles.DrawLine(   -point.transform.right * 0.25f + offset,
+                            point.transform.forward * 0.35f + offset,
+                            5f * visualMod);
+
+        offset = point.transform.position - point.transform.forward * 0.65f;
+
+        Handles.DrawLine(point.transform.right * 0.25f + offset,
+                            point.transform.forward * 0.35f + offset,
+                            5f * visualMod);
+        Handles.DrawLine(-point.transform.right * 0.25f + offset,
+                            point.transform.forward * 0.35f + offset,
+                            5f * visualMod);
+
+        Handles.color = Color.blue * visualMod;
         foreach (Waypoint cPoint in point.ConnectedWaypoints)
         {
             Handles.DrawLine(   point.transform.position,
